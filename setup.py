8d21869e44c59c46c0c298ed7f26f1ff67aa1666
Zangar <98948508+ppcreator@users.noreply.github.com>
2022-02-03 21:48:36 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='GPS',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/ppcreator',
+    license='',
+    author='ppcreator',
+    author_email='',
+    description='GPS set'
+)
